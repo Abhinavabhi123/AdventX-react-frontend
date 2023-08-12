b6f0069fbf42a744b098c49f7a1b94f2cb846d5d
Abhinav <abhinavkinarullathil@gmail.com>
2023-08-12 14:38:33 +0530

    completed search in the all activity page

--- src/components/User/AllActivities/AllActivities.tsx
+++ src/components/User/AllActivities/AllActivities.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import axios from "axios";
-
 import ActiveEventCard from "./ActiveEventCard";
+import searchGif from "/gifs/search-not-found.gif";
 
 function AllActivities() {
   const [eventData, setEventData] = useState([
@@ -13,7 +13,15 @@ function AllActivities() {
       location: "",
     },
   ]);
-  const [search,setSearch]=useState<string>("")
+  const [searchedData, setSearchedData] = useState([
+    {
+      _id: "",
+      primaryImage: "",
+      eventName: "",
+      eventType: "",
+      location: "",
+    },
+  ]);
   useEffect(() => {
     (async () => {
       await axios
@@ -21,48 +29,68 @@ function AllActivities() {
         .then((response) => {
           if (response?.data.status === 200) {
             setEventData(response?.data?.eventData);
+            setSearchedData(response?.data?.eventData);
           }
         });
     })();
   }, []);
-  console.log(search);
+  const submitSearch = () => {
+   const input = document.getElementById("searchInput")as HTMLInputElement
+    input.value=""
+  setEventData(searchedData)
+  };
 
-  const submitSearch=(e:React.FormEvent<HTMLFormElement>)=>{
-    e.preventDefault()
+  function searchData(e: React.ChangeEvent<HTMLInputElement>) {
+    const result = searchedData.filter((data) => {
+      return data?.eventName
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+    });
+    setEventData(result);
   }
-  
-  
 
   return (
     <div className="w-full h-full">
       <div className="w-full h-14  flex justify-center items-center ">
         {/* <div className="w-[25rem] h-9  border border-gray-400 rounded-full flex items-center ps-1 pe-1"> */}
-          <form className="w-[25rem] h-9  border border-gray-400 rounded-full flex items-center ps-1 pe-1" onSubmit={submitSearch}>
-            <button className="w-7 h-7 bg-white rounded-full flex justify-center items-center">
-              <img src="/icons/search.png" alt="search" className="w-5" />
-            </button>
-            <input
-              className="w-[21rem] h-8  me-1 text-xs ps-3 focus:outline-none  focus:border-transparent"
-              type="search"
-              placeholder="Search Event"
-              onChange={(e)=>{setSearch(e.target.value)}}
+        <div
+          className="w-[25rem] h-9  border border-gray-400 rounded-full flex items-center ps-1 pe-1"
+          // onSubmit={submitSearch}
+        >
+          <button className="w-7 h-7 bg-white rounded-full flex justify-center items-center">
+            <img src="/icons/search.png" alt="search" className="w-5" />
+          </button>
+          <input
+            className="w-[21rem] h-8  me-1 text-xs ps-3 focus:outline-none  focus:border-transparent"
+            type="search"
+            id="searchInput"
+            placeholder="Search Event"
+            onChange={(e) => {
+              searchData(e);
+            }}
+          />
+          <button type="reset" className="w-7 h-7 bg-slate-300 rounded-full flex justify-center items-center hover:rotate-90 transition-transform duration-300" onClick={submitSearch}>
+            <img
+              src="/icons/closeBtn.png"
+              alt="arrow"
+              className="w-4"
             />
-            <button className="w-7 h-7 bg-green-500 rounded-full flex justify-center items-center">
-              <img
-                src="/icons/right_white_arrow.png"
-                alt="arrow"
-                className="w-5"
-              />
-            </button>
-          </form>
+          </button>
+        </div>
         {/* </div> */}
       </div>
       <div className="w-full min-h-full max-h-fit bg-transparent flex justify-center mb-28">
-        <div className="w-[80%] h-full bg-transparent grid grid-cols-4 gap-6 md:grid-cols-4  sm:grid-cols-2 ">
-          {eventData.map((event, i) => {
-            return <ActiveEventCard key={i} event={event} />;
-          })}
-        </div>
+        {eventData.length !== 0 ? (
+          <div className="w-[80%] h-full bg-transparent grid grid-cols-4 gap-6 md:grid-cols-4  sm:grid-cols-2 ">
+            {eventData.map((event, i) => {
+              return <ActiveEventCard key={i} event={event} />;
+            })}
+          </div>
+        ) : (
+          <div className="w-[100vw] h-[100vh]  flex justify-center items-center">
+            <img className="w-96 h-80" src={searchGif} alt="empty data" />
+          </div>
+        )}
       </div>
     </div>
   );
